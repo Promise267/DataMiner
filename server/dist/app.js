cb8cc7c70b74439aeb2bd78e4e3d839700d6315f
Promise267 <promrizal@gmail.com>
2024-06-28 12:08:45 +0545

    typescript-compiler-added

---
+++ server/dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript with Express!');
+});
+fs_1.default.writeFileSync('text.txt', "test");
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
